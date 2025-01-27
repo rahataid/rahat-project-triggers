94c391ce2133bccab70126ae7858a46ed3ced1b4
karun-rumsan <gyawalikarrun@gmail.com>
2025-01-27 20:56:45 +0545

    seeding the glofas data

--- src/sources-data/dto/index.ts
+++ src/sources-data/dto/index.ts
@@ -1,2 +1,11 @@
 export * from './create-sources-data.dto';
 export * from './update-sources-data.dto';
+
+export interface GlofasStationInfo {
+  LOCATION: string;
+  I: string;
+  J: string;
+  URL: string;
+  BBOX: string;
+  TIMESTRING: string;
+}
