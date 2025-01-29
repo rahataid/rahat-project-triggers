365f52e0648cb696b0251dee1264fbef7c06b52e
karun-rumsan <gyawalikarrun@gmail.com>
2025-01-29 22:04:53 +0545

    listner module  and constant  and dto updated

--- src/listeners/listeners.module.ts
+++ src/listeners/listeners.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ListernersService } from './listeners.service';
 import { BullModule } from '@nestjs/bull';
 import { BQUEUE } from 'src/constant';
+import { StatsService } from 'src/stats/stats.service';
 
 @Module({
   imports: [
@@ -9,6 +10,6 @@ import { BQUEUE } from 'src/constant';
       name: BQUEUE.SCHEDULE,
     }),
   ],
-  providers: [ListernersService],
+  providers: [ListernersService, StatsService],
 })
 export class ListenersModule {}
