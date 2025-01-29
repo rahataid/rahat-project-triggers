32762a6be01190ec9c26985e7f85a5933b385ab4
karun-rumsan <gyawalikarrun@gmail.com>
2025-01-29 21:59:03 +0545

    trigger module updated

--- src/trigger/trigger.module.ts
+++ src/trigger/trigger.module.ts
@@ -1,11 +1,18 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TriggerService } from './trigger.service';
 import { TriggerController } from './trigger.controller';
 import { BullModule } from '@nestjs/bull';
 import { BQUEUE } from 'src/constant';
+import { PhasesModule } from 'src/phases/phases.module';
+import { PrismaModule } from '@rumsan/prisma';
+import { HttpModule } from '@nestjs/axios';
+import { SourcesDataModule } from 'src/sources-data/sources-data.module';
 
 @Module({
   imports: [
+    PrismaModule,
+    HttpModule,
+    SourcesDataModule,
     BullModule.registerQueue(
       {
         name: BQUEUE.SCHEDULE,
@@ -14,8 +21,10 @@ import { BQUEUE } from 'src/constant';
         name: BQUEUE.TRIGGER,
       },
     ),
+    forwardRef(() => PhasesModule),
   ],
   controllers: [TriggerController],
   providers: [TriggerService],
+  exports: [TriggerService],
 })
 export class TriggerModule {}
