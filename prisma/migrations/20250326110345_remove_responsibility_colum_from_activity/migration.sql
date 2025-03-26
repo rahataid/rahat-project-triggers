f34b238e3e54281b357b57d925b1c1224effdb60
Dipesh Kumar Sah <dipesh@rumsan.net>
2025-03-26 17:49:09 +0545

    fix: remove responsibilty column from activity

---
+++ prisma/migrations/20250326110345_remove_responsibility_colum_from_activity/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `responsibility` on the `tbl_activities` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "tbl_activities" DROP COLUMN "responsibility";
