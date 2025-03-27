4a6f41e1c726a739cf5c86014883e8193693b1af
Dipesh Kumar Sah <dipesh@rumsan.net>
2025-03-27 16:59:48 +0545

    chore: update trigger service

---
+++ prisma/migrations/20250327110020_remove_app_from_triggers/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `app` on the `tbl_triggers` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "tbl_triggers" DROP COLUMN "app";
