2f9f7189bf28beec9f16557d463394af87913567
Dipesh Kumar Sah <dipesh@rumsan.net>
2025-04-01 14:17:40 +0545

    feat: created new seed file to seed datasouce

---
+++ prisma/seed.ts
@@ -0,0 +1,53 @@
+import { PrismaService } from '@rumsan/prisma';
+import { PrismaClient, DataSource, SourceType } from '@prisma/client';
+import { SettingsService } from '@rumsan/settings';
+import { DataSourceConfig } from 'src/types/settings';
+
+const prisma = new PrismaClient();
+const prismaService = new PrismaService();
+const settings = new SettingsService(prismaService);
+
+const config: DataSourceConfig = {
+  name: 'DATASOURCE',
+  value: {
+    [DataSource.DHM]: [
+      {
+        [SourceType.RAINFALL]: {
+          LOCATION: 'Karnali at Chisapani',
+          SERIESID: 13067,
+        },
+        [SourceType.WATER_LEVEL]: {
+          LOCATION: 'Mahakali',
+          SERIESID: 29089,
+        },
+      },
+    ],
+    [DataSource.GLOFAS]: {},
+  },
+  isPrivate: false,
+};
+
+const main = async () => {
+  try {
+    const dataSource = await settings.getPublic('DATASOURCE');
+
+    if (dataSource) {
+      console.log('DATASOURCE already exists');
+      await settings.delete('DATASOURCE');
+      console.log('Old DATASOURCE deleted');
+    }
+    await settings.create(config);
+  } catch (error) {
+    await settings.create(config);
+    console.log(`New DATASOURCE created for karnali`);
+  }
+};
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (error) => {
+    console.log(error);
+    await prisma.$disconnect();
+  });
