6e7da63769ab3ecc932d8b595151d4be9e4a9cbf
Dipesh Kumar Sah <dipesh@rumsan.net>
2025-04-11 11:09:31 +0545

    fix: add retry for radis connection

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,8 @@ async function bootstrap() {
         host: process.env.REDIS_HOST,
         port: Number(process.env.REDIS_PORT),
         password: process.env.REDIS_PASSWORD,
+        retryDelay: 3000,
+        retryAttempts: 50,
       },
     },
   );
