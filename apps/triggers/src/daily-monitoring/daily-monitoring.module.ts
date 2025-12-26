a236349bc31e2c50e778d780418d5fc99f462216
amrit-rumsan <amrit@rumsan.net>
2025-12-26 14:29:04 +0545

    resolve test issues

--- apps/triggers/src/daily-monitoring/daily-monitoring.module.ts
+++ apps/triggers/src/daily-monitoring/daily-monitoring.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DailyMonitoringService } from './daily-monitoring.service';
 import { DailyMonitoringController } from './daily-monitoring.controller';
+import { PrismaModule } from '@lib/database';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [DailyMonitoringController],
   providers: [DailyMonitoringService],
 })
