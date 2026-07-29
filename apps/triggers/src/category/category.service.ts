05722cd4a5737066259c2d975c5103a7eddd0f02
nabina-rumsan <nabina.rumsan@gmail.com>
2026-07-29 14:27:50 +0545

    fix:removed pagination from category listing

--- apps/triggers/src/category/category.service.ts
+++ apps/triggers/src/category/category.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
-import { paginator, PaginatorTypes } from '@lib/database';
 import { ListCategoryDto } from './dto';
 import { PrismaService } from '@lib/database';
-
-const paginate: PaginatorTypes.PaginateFunction = paginator({ perPage: 10 });
 
 @Injectable()
 export class CategoryService {
@@ -20,47 +17,22 @@ export class CategoryService {
     });
   }
 
-  async findAll(payload: ListCategoryDto) {
-    const { appId, name, page, perPage } = payload;
+  findAll(payload: ListCategoryDto) {
+    const { appId, name } = payload;
 
     const query = {
       where: {
         app: appId,
         isDeleted: false,
-        ...(name && {
-          name: {
-            contains: name,
-            mode: 'insensitive' as const,
-          },
-        }),
+        ...(name && { name: { contains: name, mode: 'insensitive' as const } }),
       },
       orderBy: {
         // [sort]: order,
       },
     };
 
-    if (page !== undefined && perPage !== undefined) {
-      return paginate(this.prisma.activityCategory, query, {
-        page,
-        perPage,
-      });
-    }
-
-    const data = await this.prisma.activityCategory.findMany(query);
-
-    return {
-      data,
-      meta: {
-        total: data.length,
-        lastPage: 1,
-        currentPage: 1,
-        perPage: data.length,
-        prev: null,
-        next: null,
-      },
-    };
+    return this.prisma.activityCategory.findMany(query);
   }
-
   findOne(uuid: string) {
     return this.prisma.activityCategory.findUnique({
       where: {
