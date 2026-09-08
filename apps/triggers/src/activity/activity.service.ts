dec09ca7811a145fc362a00a509d5aa8b506ad95
nabina-rumsan <nabina.rumsan@gmail.com>
2026-09-08 12:23:32 +0545

    feat: add startedAt and updatedAt fields for activity communications

--- apps/triggers/src/activity/activity.service.ts
+++ apps/triggers/src/activity/activity.service.ts
@@ -454,12 +454,16 @@ export class ActivityService {
 
           let sessionStatus = SessionStatus.NEW;
           let completedAt = null;
+          let startedAt = null;
+          let updatedAt = null;
           if (communication.sessionId) {
             const sessionDetails = await this.commsClient.session.get(
               communication.sessionId,
             );
             sessionStatus = sessionDetails.data.status;
             completedAt = sessionDetails.data.updatedAt;
+            startedAt = sessionDetails.data.createdAt;
+            updatedAt = sessionDetails.data.updatedAt;
           }
           // const transport = await this.commsClient.transport.get(
           //   communication.transportId,
@@ -488,6 +492,8 @@ export class ActivityService {
             transportName: transportName,
             sessionStatus,
             completedAt,
+            startedAt,
+            updatedAt,
             ...(communication.sessionId && {
               sessionId: communication.sessionId,
             }),
